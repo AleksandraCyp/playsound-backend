80ad538820fadfc04cf3f5e2e0e693dd8622ef7f
AleksandraCyp <73715885+AleksandraCyp@users.noreply.github.com>
2021-08-08 19:27:38 +0200

    Change origin

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "*",
+    origin: "https://aleksandracyp.github.io/playsound-frontend/",
     methods: ["GET", "POST"],
   },
 });
